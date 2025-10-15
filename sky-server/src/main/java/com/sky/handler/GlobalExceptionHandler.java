984388df39bf6914bad10744cb6e2038ad6ddb42
123 <1812964997@qq.com>
2025-10-15 20:03:24 +0800

    新增员工接口

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -6,6 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.Arrays;
+
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
  */
@@ -24,4 +27,13 @@ public class GlobalExceptionHandler {
         return Result.error(ex.getMessage());
     }
 
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        if(ex.getMessage().contains("Duplicate entry")) {
+            String[] split = ex.getMessage().split(" ");
+            String msg = split[2] + "已存在";
+            return Result.error(msg);
+        }
+        return Result.error(ex.getMessage());
+    }
 }
